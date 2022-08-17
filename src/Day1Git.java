5004ea7f1bc2177aeadb55be570663ea9048259f
serapuykur <kagan.ozan1235@gmail.com>
2022-08-17 15:48:02 +0200

    V1

---
+++ src/Day1Git.java
@@ -0,0 +1,9 @@
+public class Day1Git {
+    public static void main(String[] args) {
+        System.out.println("Git 1. Deneme");
+        System.out.println("Hello git ");
+        System.out.println("Hello git 3.deneme ");
+        System.out.println("Hello git 4.deneme  guzelgit");
+    }
+}
+
