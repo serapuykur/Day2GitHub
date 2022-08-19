68950092f9242aab58ed49d9ad505d2356a41ad3
serapuykur <kagan.ozan1235@gmail.com>
2022-08-19 14:31:27 +0200

    QQQ

--- src/Day1Git.java
+++ src/Day1Git.java
@@ -6,6 +6,7 @@ public class Day1Git {
         System.out.println("Hello git 4.deneme  guzelgit");
         System.out.println("hello git 5. deneme");
          System.out.println("hello git 6. deneme");
+         System.out.println("hello git 7. deneme");
     }
 }
 
